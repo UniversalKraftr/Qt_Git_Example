ae6dbd80359ce4c12beba66badfbc5c88e8ced37
Cory Jaramillo <cory.jaramillo8@gmail.com>
2019-04-04 22:12:21 -0600

    Empty description. Testing

---
+++ widget.cpp
@@ -0,0 +1,17 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QDebug>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    qDebug() << "This is a modification";
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
